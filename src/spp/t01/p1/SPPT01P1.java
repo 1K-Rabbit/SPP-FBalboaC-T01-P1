73595b8313e44c825570a8d48a6aa4eac95e075f
FabricioArturo <FabricioArturo@192.168.1.76>
2017-01-31 08:15:19 -0600

    Problema 1.1, Tarea 1

--- src/spp/t01/p1/SPPT01P1.java
+++ src/spp/t01/p1/SPPT01P1.java
@@ -13,42 +13,54 @@ public class SPPT01P1 {
     /**
      * @param args the command line arguments
      */Scanner KB = new Scanner (System.in);
-    public static void main(String[] args) {int mat,cal1,cal2,cal3,cal4,res,pro;
-      mat=solicitarDatos("Matricula");
-      cal1=solicitarDatos("calificacion 1: ");
-      cal2=solicitarDatos("Calificacion 2: ");
-      cal3=solicitarDatos("Calificacion 3: ");
-      cal4=solicitarDatos("Calificacion 4: ");
-      pro=promedio(cal1, cal2,cal3,cal4);
-      res=resultado(pro);
-      mensaje(pro,mat);
-    }
-    public static int solicitarDatos(String d){
-        int n;
-        Scanner KB= new Scanner(System.in);
-        System.out.println("Intoduce: " + d);
-        n= KB.nextInt();
-        return n;
+    public static void main(String[] args) { 
+        Scanner Kb = new Scanner (System.in);
+        int Mat;
+        Double Cal1;
+        Double Cal2;
+        Double Cal3;
+        Double Cal4;
+        Double Cal5;
+        Double Pro;
+        String Res;
+        
+        
+        
+        System.out.println("BIENVENIDO A LA BASE DE DATOS DE ALUMNOS DE SEGUNDO SEMESTRE");
+        System.out.println("¿Cual es tu matricuala?");
+            Mat = Kb.nextInt();
+        System.out.println("Calificacion en  Bellas artes ");
+           Cal1 = Kb.nextDouble();
+        System.out.println("Calificacion en Quimica ");
+           Cal2 = Kb.nextDouble();
+        System.out.println("Calificacion en Fisica II");
+           Cal3 = Kb.nextDouble();
+        System.out.println("Calificacion en Lengua Extranjera");
+           Cal4 = Kb.nextDouble();
+        System.out.println("Calificacion en Matematicas II");
+           Cal5 = Kb.nextDouble();
        
-    }
-    public static int promedio (int cal1, int cal2, int cal3, int cal4){
-        int p;
-        p= (cal1 + cal2 +cal3 + cal4)/4;
-        return p;
-    }
-    public static int resultado(int pro){
-        if (pro>=70){
-            System.out.println("APROBADO");
+           //RES (PROMEDIO)
+           Pro = (Cal1 + Cal2 + Cal3 +Cal4 + Cal5)/5;
            
-    } else { 
-            System.out.println("REPROBADO");
-        }
-        return pro;
-    }
-    public static void mensaje(int pro, int mat){
-        System.out.println("Tu matricula es:" + mat);
-        System.out.println("Tu promedio es: " + pro);
-        System.out.println("Gracias por su atencion");
+           
+           //PROM (APROBADO / REPROBADO)
+            
+                    if (Pro >= 7){
+               Res = ("APROBATORIA");
+                       }
+                       else {
+  
+               Res = ("REPROBATORIA");
+                               } 
+           
+           
+           
+           System.out.println("El alumno con matricula " + Mat + " tiene un promedio de " + Pro + " por lo que tiene una calificacion " + Res + " en este semestre.");
+                   
+           
+            
+        
         
         
     }
